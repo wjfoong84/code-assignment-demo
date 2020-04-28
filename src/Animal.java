dadae562f367c1637129338575ae9f64c28deda4
Foong WenJun <wjfoong84@gmail.com>
2020-04-28 23:26:44 +0800

    initial commit

---
+++ src/Animal.java
@@ -0,0 +1,8 @@
+
+public class Animal {
+
+	void walk(){
+		System.out.println("I am walking");
+	}
+	
+}
---
+++ src/Bird.java
@@ -0,0 +1,8 @@
+
+public class Bird extends Animal {
+	
+	void fly() {
+		System.out.println("I am flying");
+	}
+	
+}
